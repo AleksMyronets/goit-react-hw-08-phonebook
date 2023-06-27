dd854d2ffe28374c7b247364355d7adfa4136b8d
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-06-28 00:22:46 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,45 @@
-import { Contacts } from '../Contacts/Contacts';
-import { Filter } from '../Filter/Filter';
-import { Forma } from '../Forma/Forma';
-import { ContainerForm } from './App.styled';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Contacts from '../Pages/Contacts';
+import Login from '../Pages/LoginUser';
+import Header from 'components/Header/Header';
+import Register from '../Pages/Register';
+import PrivateRouter from './PrivateRouter/PrivateRouter';
+import PublicRouter from './PublicRouter/PublicRouter';
+// import { ContainerForm } from './App.styled';
 
 export const App = () => {
   return (
-    <ContainerForm>
-      <h1>Phonebook</h1>
-      <Forma />
-      <h2>Contacts</h2>
-      <Filter />
-      <Contacts />
-    </ContainerForm>
+    <>
+      <Toaster />
+      <Header />
+      <Routes>
+        <Route
+          path="/register"
+          element={
+            <PublicRouter>
+              <Register />
+            </PublicRouter>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PublicRouter>
+              <Login />
+            </PublicRouter>
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRouter>
+              <Contacts />
+            </PrivateRouter>
+          }
+        />
+        <Route path="*" element={<Navigate to={'/login'} />} />
+      </Routes>
+    </>
   );
 };
-
