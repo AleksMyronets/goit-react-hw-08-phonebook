3cc5d2bceca827bcb401b9d2327f79f6e49a77cf
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-06-30 11:37:35 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,11 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import Contacts from '../Contacts/Contacts';
+import Contacts from '../Pages/Contacts';
 import Login from '../Pages/LoginUser';
 import Header from '../Header/Header';
 import Register from '../Pages/RegisterUser';
 import PrivateRouter from '../PrivateRouter/PrivateRouter';
 import PublicRouter from '../PublicRouter/PublicRouter';
-// import { ContainerForm } from './App.styled';
 
 export const App = () => {
   return (
