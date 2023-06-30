ff7b762e1e826b57095664e9f7dfbb1479aa7f0e
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-06-30 11:23:55 +0300

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,8 +1,25 @@
 import { contactReducer } from './sliceContact';
 import { filterReducer } from './sliseFilter';
 import { combineReducers } from 'redux';
+import { authReducer} from "./sliceUser"
+
+import {persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+
+const persistConfig = {
+  key: 'token',
+  storage,
+  whitelist:["token"]
+}
+
+const persistedReducer = persistReducer(persistConfig, authReducer)
+
+
 
 export const reducer = combineReducers({
   contacts: contactReducer,
   filter: filterReducer,
+  auth: persistedReducer,
 });
+
