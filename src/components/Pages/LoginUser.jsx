b95cc0458bf2d74ffd7c09f7ff2f57b1fc8ba44c
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-06-30 11:17:22 +0300

    Update

---
+++ src/components/Pages/LoginUser.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
+import { loginThunk } from 'redux/sliceUser';
+import css from './LoginUser.module.css';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const onSubmit = e => {
+    e.preventDefault();
+    dispatch(loginThunk({ email, password }))
+      .unwrap()
+      .then(() => toast.success('Welcome'), navigate('/contacts'))
+      .catch(error => toast.error(`Wrong password or email`));
+  };
+
+  const onChange = ({ target: { value, name } }) => {
+    name === 'email' ? setEmail(value) : setPassword(value);
+  };
+
+  return (
+    <>
+      <form autoComplete="on" onSubmit={onSubmit} className={css.LoginForm}>
+        <label className={css.LoginLabel}>
+          Email address
+          <input
+            type="email"
+            name="email"
+            id="examplInputEmail"
+            onChange={onChange}
+            required
+            className={css.LoginInput}
+          />
+        </label>
+
+        <label className={css.LoginLabel}>
+          Password
+          <input
+            type="password"
+            name="password"
+            id="examplInputPassword"
+            onChange={onChange}
+            required
+            className={css.LoginInput}
+          />
+        </label>
+
+        <button type="submit" className={css.LoginBtn}>
+          Login
+        </button>
+      </form>
+    </>
+  );
+};
+export default Login;
+
---
+++ src/components/Pages/RegisterUser.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { register } from 'api';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
+import css from './RegisterUser.module.css';
+
+const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  const navigate = useNavigate();
+
+  const onSubmit = e => {
+    e.preventDefault();
+    register({
+      name,
+      email,
+      password,
+    })
+      .then(() => navigate('/login'))
+      .catch(error => toast.error(`Data entered incorrectly`));
+  };
+
+  const onChange = ({ target: { value, name } }) => {
+    name === 'email'
+      ? setEmail(value)
+      : name === 'password'
+      ? setPassword(value)
+      : setName(value);
+  };
+
+  return (
+    <>
+      <form autoComplete="on" onSubmit={onSubmit} className={css.RegisterForm}>
+        <label className={css.RegisterLabel}>
+          Name
+          <input
+            type="text"
+            name="name"
+            id="examplInputName"
+            onChange={onChange}
+            required
+            className={css.RegisterInput}
+          />
+        </label>
+        <label className={css.RegisterLabel}>
+          Email address
+          <input
+            type="email"
+            name="email"
+            id="examplInputEmail"
+            onChange={onChange}
+            required
+            className={css.RegisterInput}
+          />
+        </label>
+        <label className={css.RegisterLabel}>
+          Password
+          <input
+            type="password"
+            name="password"
+            id="examplInputPassword"
+            onChange={onChange}
+            required
+            className={css.RegisterInput}
+          />
+        </label>
+
+        <button type="submit" className={css.RegisterBtn}>
+          Register
+        </button>
+      </form>
+    </>
+  );
+};
+export default Register;
