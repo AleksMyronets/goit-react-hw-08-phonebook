19d54b14d80bce3d647055e2c9db214dd76bf2ab
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-07-01 10:48:21 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,3 +1,55 @@
+// import axios from 'axios';
+
+// const instance = axios.create({
+//   baseURL: 'https://connections-api.herokuapp.com/',
+// });
+
+// export const setToken = token => {
+//   instance.defaults.headers.common['Authorization'] = token;
+// };
+
+// const dellToken = () => {
+//   instance.defaults.headers.common['Authorization'] = "";
+// };
+
+// export const register = async body => {
+//   const { data } = await instance.post('/users/signup', body);
+//   setToken(`Bearer ${data.token}`);
+//   return data
+// };
+
+// export const login = async body => {
+//   const { data } = await instance.post('/users/login', body);
+//   setToken(`Bearer ${data.token}`);
+//   return data;
+// };
+
+// export const getCurrent = async () => {
+//   const { data } = await instance.get('/users/current')
+//   return data;
+// };
+
+// export const logout = async () => {
+//   await instance.post('/users/logout')
+//   dellToken()
+// };
+
+
+// export const getContacts = async () => {
+//   const { data } = await instance.get('/contacts');
+//   return data;
+// };
+
+// export const addContacts = async({name, number}) => {
+//   const { data } = await instance.app('/contacts', {name, number});
+//   return data;
+// };
+
+// export const deleteContact = async id => {
+//   const { data } = await instance.delete(`/contacts/${id}`);
+//   return data;
+// };
+
 import axios from 'axios';
 
 const instance = axios.create({
@@ -36,12 +88,12 @@ export const logout = async () => {
 
 
 export const getContacts = async () => {
-  const { data } = await instance.get('/contacts');
+  const { data } = await instance.get("/contacts");
   return data;
 };
 
-export const addContacts = async({name, phone}) => {
-  const { data } = await instance.app('/contacts', {name, phone});
+export const addContacts = async post => {
+  const { data } = await instance.post("/contacts",post);
   return data;
 };
 
